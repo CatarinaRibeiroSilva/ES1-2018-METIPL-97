a48ea99dce607b55d0934560f497e4122aaeea1d
Catarina Silva <Catarina Silva@AcerTravelMate.home>
2018-11-22 01:07:20 +0000

    merge with master

---
+++ projecto/src/projecto/Publicacao.java
@@ -0,0 +1,44 @@
+package projecto;
+
+public class Publicacao implements Comparable <Publicacao>{
+	private String tipo ;
+	private String origem;
+	private String mensagem;
+	private String titulo;
+	private String data;
+
+	public Publicacao (String tipo, String origem, String mensagem, String data)  {
+		this.tipo=tipo;
+		this.origem=origem;
+		this.mensagem=mensagem;
+		this.data=data;
+		titulo=mensagem.substring(0, 15);
+		}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public String getOrigem() {
+		return origem;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	@Override
+	public int compareTo(Publicacao arg0) {
+	
+		return 0;
+	}
+
+}
